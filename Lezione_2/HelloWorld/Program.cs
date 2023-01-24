bcf6cc10b27c68e3b482f219491a4fef16d54d68
alessiofilippucci <info@alessiofilippucci.it>
2023-01-24 08:54:06 +0100

    First commit

---
+++ Lezione_2/HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿// namespace which contains the commonly used types
+using System;
+
+// namespace declaration
+namespace HelloWorld
+{
+    // Class declaration
+    internal class Program
+    {
+        // Main Method
+        static void Main(string[] args)
+        {
+            // SONO UNA RIGA DI COMMENTO
+            // SONO UN'ALTRA RIGA DI COMMENTO
+
+            /* SONO UN COMMENTO
+             * SU
+             * PIU'
+             * RIGHE
+             */
+
+            // statement
+            // printing Hello World!
+            Console.WriteLine("Hello World!");
+
+            // To prevents the screen from 
+            // running and closing quickly
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lezione_2/Literals/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+
+namespace Literals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // decimal-form literal
+            int decimalFormLiteral = 101;
+
+            // octal-form literal
+            int octalFormLiteral = 0145;
+
+            // Hexa-decimal form literal
+            int hexaDecimalFormLiteral = 0xFace;
+
+            // binary-form literal
+            int binaryFormLiteral = 0b101;
+
+            // floating-point literal
+            double floatingPointLiteral = 101.230;
+
+            // It also acts as floating-point literal
+            double floatingPointLiteral2 = 0123.222;
+
+            // character literal within single quote
+            char characterFormLiteral = 'a';
+
+            // Unicode representation
+            char characterUnicodeFormLiteral = '\u0061';
+
+            // Escape character literal
+            string characterEscapeFormLiteral = "Hello,\nWorld!";
+
+            // boolean literal
+            bool booleanLiteral = true;
+            bool booleanLiteral2 = false;
+
+            // these will give compile time error
+            // bool booleanLiteral3 = 0;
+            // bool booleanLiteral4 = 1;
+            // Console.WriteLine(booleanLiteral3);
+            // Console.WriteLine(booleanLiteral4);
+
+            Console.WriteLine(decimalFormLiteral);
+            Console.WriteLine(octalFormLiteral);
+            Console.WriteLine(hexaDecimalFormLiteral);
+            Console.WriteLine(binaryFormLiteral);
+            Console.WriteLine(floatingPointLiteral);
+            Console.WriteLine(floatingPointLiteral2);
+            Console.WriteLine(characterFormLiteral);
+            Console.WriteLine(characterUnicodeFormLiteral);
+            Console.WriteLine(characterEscapeFormLiteral);
+            Console.WriteLine(booleanLiteral);
+            Console.WriteLine(booleanLiteral2);
+        }
+    }
+}
