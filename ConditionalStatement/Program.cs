2f58dc24d22238655d8944b2a1647c9543937d76
alessiofilippucci <info@alessiofilippucci.it>
2023-01-24 10:25:38 +0100

    IF

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ConditionalStatement
+﻿using System.Globalization;
+
+namespace ConditionalStatement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inserisci il tuo nome");
+
+            string nome;
+
+            nome = Console.ReadLine();
+
+            if (nome == "Alessio")
+            {
+                Console.WriteLine("Ehi ciao, bentornato!!");
+
+                Console.WriteLine("Adesso inserisci la tua data di nascita");
+
+                DateTime dob;
+
+                string dobText = Console.ReadLine();
+
+                try
+                {
+                    dob = DateTime.ParseExact(dobText, "MM yyyy dd", CultureInfo.InvariantCulture);
+
+                    // DateTime.Now => data attuale
+
+                    if (scrivi condizione)
+                    {
+                        Console.WriteLine("Ok " + eta + ", puoi continuare...");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Non è un numero valido '" + dobText + "'");
+                }
+            }
         }
     }
 }
