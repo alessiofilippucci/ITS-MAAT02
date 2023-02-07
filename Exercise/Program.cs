0900126ded10948c199e43e0e7adbdb5013992d9
alessiofilippucci <info@alessiofilippucci.it>
2023-02-07 09:10:40 +0100

    Added code

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -4,7 +4,106 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> allowedOps = new List<string>() {
+                "+",
+                "-",
+                "*",
+                "/"
+            };
+
+            var restart = true;
+
+            while (restart)
+            {
+                int? num1 = null;
+                int? num2 = null;
+                string operazione;
+                int? result = null;
+
+                List<int> nums = new List<int>();
+
+                bool ended = false;
+
+                Console.WriteLine("Inserisci dei numeri. Per finire l'inserimento non digitare nulla.");
+
+                while (!ended)
+                {
+                    do
+                    {
+                        try
+                        {
+                            string value = Console.ReadLine();
+
+                            if (value == "")
+                            {
+                                ended = true;
+                                break;
+                            }
+
+                            num1 = int.Parse(value);
+                            nums.Add(num1.Value);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Il numero inserito non è valido");
+                        }
+                    }
+                    while (num1 == null);
+                }
+
+                // VERIFICA L'OPERAZIONE
+
+                do
+                {
+                    Console.WriteLine("Inserisci il l'operazione: (+, -, *, /)");
+                    operazione = Console.ReadLine();
+
+                    if (allowedOps.Contains(operazione))
+                    {
+                        switch (operazione)
+                        {
+                            case "+":
+                                {
+                                    result = num1 + num2;
+                                    break;
+                                }
+                            case "-":
+                                {
+                                    result = num1 - num2;
+                                    break;
+                                }
+                            case "*":
+                                {
+                                    result = num1 * num2;
+                                    break;
+                                }
+                            case "/":
+                                {
+                                    result = num1 / num2;
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("Operazione '" + operazione + "' non gestita");
+                                    break;
+                                }
+                        }
+                    }
+
+                }
+                while (operazione != "+" && operazione != "-" && operazione != "*" && operazione != "/");
+
+
+                if (result != null)
+                {
+                    Console.WriteLine($"{num1} {operazione} {num2} = {result}");
+                }
+
+
+                Console.WriteLine($"Vuoi effettuare una nuova operazione? 1 = si, 2 = no");
+
+                restart = Console.ReadLine() == "1";
+            }
         }
     }
 }
---
+++ Method/Program - Copy.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Method
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("APPLICATION STARTED");
+
+            Start();
+
+            Console.WriteLine("APPLICATION ENDED");
+        }
+
+        static void Start()
+        {
+            counter++;
+
+            Console.WriteLine($"Start {counter} opened");
+
+            var text = Console.ReadLine();
+
+            if (text == "")
+                return;
+
+            int lenght = CalcolaLunghezza(text);
+
+            Console.WriteLine(lenght);
+
+            Start();
+
+            Console.WriteLine($"Start {counter} closed");
+        }
+
+        static int CalcolaLunghezza(string text)
+        {
+            return text.Length;
+        }
+    }
+}
