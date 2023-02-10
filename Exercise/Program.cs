44b7450f312401fdc84f19f8d14f0e74b504af0b
alessiofilippucci <info@alessiofilippucci.it>
2023-02-10 13:26:29 +0100

    updated

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,109 +1,157 @@
-﻿namespace Exercise
+﻿using System;
+
+namespace Exercise
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            List<string> allowedOps = new List<string>() {
+        static List<string> allowedOps = new List<string>() {
                 "+",
                 "-",
                 "*",
                 "/"
             };
 
-            var restart = true;
+        static void Main(string[] args)
+        {
+            Start();
+        }
 
-            while (restart)
+        static void Start()
+        {
+            int? result = null;
+
+            List<int> numbers = GetNumbers();
+            string operazione = GetOperation();
+
+            for (int i = 0; i < numbers.Count; i++)
             {
-                int? num1 = null;
-                int? num2 = null;
-                string operazione;
-                int? result = null;
-
-                List<int> nums = new List<int>();
-
-                bool ended = false;
-
-                Console.WriteLine("Inserisci dei numeri. Per finire l'inserimento non digitare nulla.");
-
-                while (!ended)
+                if (i == 0)
                 {
-                    do
-                    {
-                        try
-                        {
-                            string value = Console.ReadLine();
-
-                            if (value == "")
-                            {
-                                ended = true;
-                                break;
-                            }
-
-                            num1 = int.Parse(value);
-                            nums.Add(num1.Value);
-                        }
-                        catch
-                        {
-                            Console.WriteLine("Il numero inserito non è valido");
-                        }
-                    }
-                    while (num1 == null);
+                    result = numbers[i];
+                    continue;
                 }
 
-                // VERIFICA L'OPERAZIONE
+                if (!Calculate(operazione, numbers[i], ref result))
+                {
+                    goto DivNotValid;
+                }
+            }
+
+            if (result != null)
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+
+        DivNotValid:
+            {
+                result = null;
+                Console.WriteLine("Divisione non valida");
+            }
+
+            Console.WriteLine($"Vuoi effettuare una nuova operazione? 1 = si, 2 = no");
+
+            if (Console.ReadLine() == "1")
+                Start();
+        }
+
+        static List<int> GetNumbers()
+        {
+            List<int> nums = new List<int>();
+
+            bool ended = false;
+
+            Console.WriteLine("Inserisci dei numeri.");
+
+            while (!ended)
+            {
+                int? num = null;
 
                 do
                 {
-                    Console.WriteLine("Inserisci il l'operazione: (+, -, *, /)");
-                    operazione = Console.ReadLine();
-
-                    if (allowedOps.Contains(operazione))
+                    try
                     {
-                        switch (operazione)
+                        string value = Console.ReadLine();
+
+                        if (value == "")
                         {
-                            case "+":
-                                {
-                                    result = num1 + num2;
-                                    break;
-                                }
-                            case "-":
-                                {
-                                    result = num1 - num2;
-                                    break;
-                                }
-                            case "*":
-                                {
-                                    result = num1 * num2;
-                                    break;
-                                }
-                            case "/":
-                                {
-                                    result = num1 / num2;
-                                    break;
-                                }
-                            default:
-                                {
-                                    Console.WriteLine("Operazione '" + operazione + "' non gestita");
-                                    break;
-                                }
+                            ended = true;
+                            break;
                         }
+
+                        num = int.Parse(value);
+                        nums.Add(num.Value);
                     }
-
+                    catch
+                    {
+                        Console.WriteLine("Il numero inserito non è valido");
+                    }
                 }
-                while (operazione != "+" && operazione != "-" && operazione != "*" && operazione != "/");
-
-
-                if (result != null)
-                {
-                    Console.WriteLine($"{num1} {operazione} {num2} = {result}");
-                }
-
-
-                Console.WriteLine($"Vuoi effettuare una nuova operazione? 1 = si, 2 = no");
-
-                restart = Console.ReadLine() == "1";
+                while (num == null);
             }
+
+            return nums;
+        }
+
+        static string GetOperation()
+        {
+            string operazione;
+
+            do
+            {
+                Console.WriteLine("Inserisci il l'operazione: (+, -, *, /)");
+                operazione = Console.ReadLine();
+            }
+            while (!allowedOps.Contains(operazione));
+
+            return operazione;
+        }
+
+        static bool Calculate(string operazione, int num, ref int? result)
+        {
+            bool completed = true;
+
+            switch (operazione)
+            {
+                case "+":
+                    {
+                        result = Sum(result.Value, num);
+                        break;
+                    }
+                case "-":
+                    {
+                        result = result - num;
+                        break;
+                    }
+                case "*":
+                    {
+                        result = result * num;
+                        break;
+                    }
+                case "/":
+                    {
+                        if ((result == 0 && num == 0) || num == 0)
+                        {
+                            completed = false;
+                            break;
+                        }
+
+                        result = result / num;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Operazione '" + operazione + "' non gestita");
+                        break;
+                    }
+            }
+
+
+            return completed;
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
